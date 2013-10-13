b72d5fa1920e4a3bb905706aeeb43e928fe34cc1
Sergei Jakovlev <sj10@ut.ee>
2013-10-14 00:36:12 +0300

    Enable font antialiasing.

--- trunk/src/ee/ut/math/tvt/flux/Intro.java
+++ trunk/src/ee/ut/math/tvt/flux/Intro.java
@@ -3,6 +3,8 @@ package ee.ut.math.tvt.flux;
 public class Intro {
 
 	public static void main(String[] args) {
+		System.setProperty("awt.useSystemAAFontSettings","on");
+		System.setProperty("swing.aatext", "true");
 		new IntroUI();
 	}
 
