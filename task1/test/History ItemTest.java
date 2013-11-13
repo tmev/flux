6051e76a4a8b635c5d20fded9d60762844044877
anastasiabolotnikova <bolotnikova.anastasia@gmail.com>
2013-11-13 19:08:13 +0200

    Tests

---
+++ task1/test/History ItemTest.java
@@ -0,0 +1,23 @@
+package ee.ut.math.tvt;
+import org.junit.Before;
+import org.junit.Test;
+
+public class HistoryItemTest {
+
+
+    @Test
+    public void testAddHistotyItem() {
+    }
+    
+    @Test
+    public void testGetSumWithNoItems() {
+    }
+
+    @Test
+    public void testGetSumWithOneItem() {
+    }
+    
+    @Test
+    public void testGetSumWithMultipleItems() {
+    }
+}
