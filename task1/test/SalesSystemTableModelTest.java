8656111c9056c51e44535af330614eef1cd8b634
Sergei Jakovlev <sj10@ut.ee>
2013-11-24 14:07:52 +0200

    [JUnit] Implement SalesSystemTableModelTest via mock implementations.

--- task1/test/SalesSystemTableModelTest.java
+++ task1/test/SalesSystemTableModelTest.java
@@ -1,61 +1,123 @@
-
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.NoSuchElementException;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import ee.ut.math.tvt.salessystem.domain.data.StockItem;
+import ee.ut.math.tvt.salessystem.domain.data.DisplayableItem;
 import ee.ut.math.tvt.salessystem.ui.model.SalesSystemTableModel;
-import ee.ut.math.tvt.salessystem.ui.model.StockTableModel;
 
 public class SalesSystemTableModelTest {
-	
-	private StockItem sti = new StockItem((long)1, "X", "Y", 5.16);
-	private StockItem sti2 = new StockItem((long)2, "Z", "W", 2.44);
 
-	
-	@Test
-	public void testGetItemById(){	
-		StockTableModel stm = new StockTableModel();
-		stm.addItem(sti);
-		SalesSystemTableModel sstm = stm;		
-		assertEquals((StockItem) sstm.getItemById(sti.getId()),sti);
-		
+	private class DisplayableItemImpl implements DisplayableItem {
+
+		private long id;
+		private String name;
+
+		public DisplayableItemImpl(long id, String name) {
+			super();
+			this.id = id;
+			this.name = name;
+		}
+
+		@Override
+		public Long getId() {
+			return id;
+		}
+
+		@Override
+		public String getName() {
+			return name;
+		}
+
 	}
-	
+
+	private class SalesSystemTableModelImpl extends
+			SalesSystemTableModel<DisplayableItem> {
+
+		private static final long serialVersionUID = 1938973502677613238L;
+
+		public SalesSystemTableModelImpl(String[] headers) {
+			super(headers);
+		}
+
+		@Override
+		protected Object getColumnValue(DisplayableItem item, int columnIndex) {
+			return item.getId();
+		}
+
+	}
+
+	DisplayableItem i1;
+	DisplayableItem i2;
+	ArrayList<DisplayableItem> l;
+	private SalesSystemTableModelImpl sstm;
+
+	@Before
+	public void setUp() {
+		i1 = new DisplayableItemImpl(1, "TEST1");
+		i2 = new DisplayableItemImpl(2, "TEST2");
+		sstm = new SalesSystemTableModelImpl(new String[] { "A", "B", "C" });
+		l = new ArrayList<>();
+		l.add(i1);
+		l.add(i2);
+		sstm.populateWithData(l);
+	}
+
+	@Test
+	public void testGetColumnCount() {
+		assertEquals(3, sstm.getColumnCount());
+	}
+
+	@Test
+	public void testGetColumnName() {
+		assertTrue("B".equals(sstm.getColumnName(1)));
+	}
+
+	@Test
+	public void testGetRowCount() {
+		assertEquals(2, sstm.getRowCount());
+	}
+
+	@Test
+	public void testGetValueAt() {
+		assertEquals(i2.getId(), sstm.getValueAt(1, 0));
+	}
+
+	@Test
+	public void testGetItemById() {
+		assertEquals(i2, sstm.getItemById(i2.getId()));
+	}
+
 	@Test
 	public void testGetItemByName() {
-		StockTableModel stm = new StockTableModel();
-		stm.addItem(sti);
-		stm.addItem(sti2);
-		SalesSystemTableModel sstm = stm;				
-		assertEquals((StockItem) sstm.getItemByName(sti.getName()),sti);
-		
+		assertEquals(i2, sstm.getItemByName(i2.getName()));
 	}
-	
+
 	@Test(expected = NoSuchElementException.class)
-	public void testGetItemByNonExistingId() {	
-		StockTableModel stm = new StockTableModel();		
-		SalesSystemTableModel sstm = stm;
-   		sstm.getItemById(2);	
+	public void testGetItemByNonExistingId() {
+		sstm.getItemById(123456);
 	}
-	
+
 	@Test(expected = NoSuchElementException.class)
 	public void testgetItemByNonExistingName() {
-		StockTableModel stm = new StockTableModel();
-		SalesSystemTableModel sstm = stm;
-   		sstm.getItemByName("WakaWaka");
-		
+		sstm.getItemByName("WakaWaka");
+
 	}
+
+	@Test
+	public void testGetTableRows() {
+		assertEquals(l, sstm.getTableRows());
+	}
+
 	@Test(expected = NoSuchElementException.class)
 	public void testClear() {
-		StockTableModel stm = new StockTableModel();
-		stm.addItem(sti);
-		stm.addItem(sti2);
-		
-		SalesSystemTableModel sstm = stm;		
+		SalesSystemTableModelImpl sstm = new SalesSystemTableModelImpl(null);
+		sstm.populateWithData(l);
+
 		sstm.clear();
-   		sstm.getItemByName(sti.getName());				
+		sstm.getItemByName(i1.getName());
 	}
 }
